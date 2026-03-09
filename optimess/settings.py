1a04713c1ca68825ae9fa7f42b4de615a149d57a
Deva Nanda <devanandasmanoj@gmail.com>
2026-03-09 16:15:46 +0530

    deployment settings fix

--- optimess/settings.py
+++ optimess/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5i$!dmj^ognb0go1dq0c*j1mxfk4fjp4t*%!_j)i+=-y*t&dg7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition

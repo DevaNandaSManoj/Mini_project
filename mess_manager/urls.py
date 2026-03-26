033ead216c5791da05f78fe57356bc76de59d209
Deva Nanda <devanandasmanoj@gmail.com>
2026-03-26 14:58:17 +0530

    mess manager frst three menus

--- mess_manager/urls.py
+++ mess_manager/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name='mess_dashboard'),
+    path('', views.mess_dashboard, name='mess_dashboard'),
+    path('edit-menu/', views.edit_menu, name='edit_menu'),
+    path('statistics/', views.meal_statistics, name='meal_statistics'),
 ]

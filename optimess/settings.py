a04ae572ed363e67ee31370fc6f5041c83437fa5
Amritha <sanjivamritha@gmail.com>
2026-04-05 21:43:27 +0530

    changes

--- optimess/settings.py
+++ optimess/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'optimess.middleware.NoCacheMiddleware',   # prevent back-button to login (must be after AuthenticationMiddleware)
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
